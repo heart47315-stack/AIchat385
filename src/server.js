cd23a9e7757fe59d2ea30f34f35f9f9f2c298516
heart47315-stack <heart47315@gmail.com>
2026-03-16 09:06:52 +0700

    1111

--- src/server.js
+++ src/server.js
@@ -27,3 +27,22 @@ initChatSocket(io)
 server.listen(3000, () => {
   console.log("Server running on port 3000")
 })
+
+const express = require("express")
+const cors = require("cors")
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req,res)=>{
+  res.send("Backend running")
+})
+
+app.post("/chat",(req,res)=>{
+  const message = req.body.message
+  res.json({reply: "AI ตอบ: "+message})
+})
+
+app.listen(3000,()=>{
+  console.log("Server running on port 3000")
+})
