53a8a22d9f7fe4f6b5accded1aff1a0555d41548
heart47315-stack <heart47315@gmail.com>
2026-03-16 13:54:56 +0700

    111

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aichat.app',
+  appName: 'aichat',
+  webDir: 'aichat-frontend/dist'
+};
+
+export default config;
