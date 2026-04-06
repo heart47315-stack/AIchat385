d20b804f86629f0e612cf398bbfc08c1c62d7ffc
heart47315-stack <heart47315@gmail.com>
2026-04-06 09:15:01 +0700

    111

--- aichat-frontend/tailwind.config.js
+++ aichat-frontend/tailwind.config.js
@@ -1,7 +1,10 @@
 export default {
-  darkMode: "class", // ❗ ต้องมี
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
+  plugins: [],
 }
